0b881e628291a8a0beee773d3fe6bd6d62335c6f
haeryung <haeryung8@gmail.com>
2024-02-21 10:11:45 +0900

    [OHH-32] 원본 레포 변경사항[ICT-004] 반영

--- api.py
+++ api.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI, HTTPException
+#api.py
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from pydantic import BaseModel
 from redis import Redis
 from rq import Queue
-
 from worker import runTask
 
 app = FastAPI()
 
-redis_conn = Redis(host='test_redis', port=6379)
+redis_conn = Redis(host='redis_queue', port=6379)
 q = Queue('my_queue', connection=redis_conn)
+
+@app.get("/run-worker-task")
+async def trigger_worker_task(background_tasks: BackgroundTasks):
+    background_tasks.add_task(runTask, redis_conn)
+    return {"message": "Worker task started in the background."}
 
 @app.get('/hello')
 def hello():
--- worker.py
+++ worker.py
@@ -1,39 +1,41 @@
+# worker.py
+
 import asyncio
 import random
 import time
 from uuid import uuid4
 from redis import Redis
 
-redis_conn = Redis(host='test_redis', port=6379)
+redis_conn = Redis(host='redis_queue', port=6379)
 
-async def generate_objects():
+async def generate_objects(redis_conn):
     for _ in range(50):
         key = str(uuid4())
         value = int(time.time())
         redis_conn.setex(key, 10, value)
         await asyncio.sleep(random.uniform(0.1, 1))
 
-async def delete_random_objects():
+async def delete_random_objects(redis_conn):
     keys = redis_conn.keys()
     num_to_delete = random.randint(0, min(10, len(keys)))
     keys_to_delete = random.sample(keys, num_to_delete)
     if keys_to_delete:
         redis_conn.delete(*keys_to_delete)
 
-def count_objects():
+def count_objects(redis_conn):
     return len(redis_conn.keys())
 
-def get_objects(limit: int):
+def get_objects(limit: int, redis_conn):
     keys = redis_conn.keys()
     keys_to_return = random.sample(keys, min(limit, len(keys)))
     return {key.decode(): redis_conn.get(key).decode() for key in keys_to_return}
 
-async def runTask():
-    await generate_objects()
-    await delete_random_objects()
-    num_objects = count_objects()
-    objects = get_objects(num_objects)
-    print(objects)
-
-if __name__ == "__main__":
-    asyncio.run(runTask())
+async def runTask(redis_conn):
+    try:
+        await generate_objects(redis_conn)
+        await delete_random_objects(redis_conn)
+        num_objects = count_objects(redis_conn)
+        objects = get_objects(num_objects, redis_conn)
+        print(objects)
+    except Exception as e:
+        print(f"An error occurred: {e}")
