e1fffcb5a23712359972b9ec3a36fb213c213a37
haeryung <haeryung8@gmail.com>
2024-02-22 14:35:13 +0900

    [OHH-30, 37] README 수정, schema 수정

--- api.py
+++ api.py
@@ -15,8 +15,17 @@ class WorkerResponse(BaseModel):
 redis_conn = Redis(host='redis_queue', port=6379)
 q = Queue('my_queue', connection=redis_conn)
 
-@app.get("/run-worker-task", response_model=WorkerResponse)
+@app.get("/", response_model=WorkerResponse)
 async def trigger_worker_task(background_tasks: BackgroundTasks):
+    """
+    Worker task를 실행하여 Redis에 객체를 생성하고 삭제합니다.
+
+    Parameters:
+    - background_tasks (BackgroundTasks): 백그라운드 작업을 처리하기 위한 FastAPI의 BackgroundTasks 객체입니다.
+
+    Returns:
+    - dict: 실행된 worker task에 대한 메시지와 생성된 객체들의 정보가 포함된 딕셔너리입니다.
+    """
     objects = await runTask(redis_conn)
     return {"message": "Worker task started in the background.", "objects": objects}
 
--- worker.py
+++ worker.py
@@ -37,6 +37,7 @@ async def runTask(redis_conn):
         num_objects = count_objects(redis_conn)
         objects = get_objects(num_objects, redis_conn)
         print(objects)
+        print(num_objects)
         return objects
     except Exception as e:
         print(f"An error occurred: {e}")
